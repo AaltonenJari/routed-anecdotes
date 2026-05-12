d580ee8ae91bd27509dfb1f2ab1be64d41ef8e91
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-05-12 19:06:30 +0300

    refraktorointi

---
+++ src/components/CreateNew.jsx
@@ -0,0 +1,42 @@
+import { useField, useReset } from '../hooks'
+
+const CreateNew = (props) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+  const resetFields = useReset([content, author, info])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button>create</button>
+        <button type='button' onClick={resetFields}>reset</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
